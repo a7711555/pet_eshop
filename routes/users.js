69c0dff57a1f94a15fda2a6eb8606d94ba609155
a7711555 <toby19940927@gmail.com>
2019-06-04 02:13:43 +0800

    Add checkout page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const area_data = require('../models/area_data');
 
-/* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/checkout', function (req, res) {
+  res.render('checkout');
+});
+
+
+router.post('/checkout/data/:city', function (req, res) {
+  const city = req.param('city');
+  const citydecode = decodeURI(Buffer.from(city, 'base64').toString('utf8'));  
+  res.send({
+    sccuess: true,
+    data: area_data[citydecode],
+  });
+  res.end();
 });
 
 module.exports = router;
