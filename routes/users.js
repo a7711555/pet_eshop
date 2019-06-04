f7c656f29ec88475ea52ade05b48f59ccb1db63c
a7711555 <toby19940927@gmail.com>
2019-06-04 22:56:47 +0800

    add order page

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ router.get('/checkout', function (req, res) {
   res.render('checkout');
 });
 
-
+// area infomation api
 router.post('/checkout/data/:city', function (req, res) {
   const city = req.param('city');
   const citydecode = decodeURI(Buffer.from(city, 'base64').toString('utf8'));  
@@ -19,6 +19,11 @@ router.post('/checkout/data/:city', function (req, res) {
     data: area_data[citydecode],
   });
   res.end();
+});
+
+// get user info
+router.post('/checkout/confirm', function(req, res){
+  res.render('confirm');
 });
 
 module.exports = router;
