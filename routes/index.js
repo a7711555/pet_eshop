3c6fb6dc98727ef102f9155d00a535700e65ec89
a7711555 <toby19940927@gmail.com>
2019-06-05 18:59:53 +0800

    new image

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const firedb = require('../models/firebaseAdmin');
+const fireDb = require('../models/firebaseAdmin');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+
   res.render('index');
 });
 
