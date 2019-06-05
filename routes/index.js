aae74972d40642f21fb17639259f981a8764abbd
a7711555 <toby19940927@gmail.com>
2019-06-06 01:06:13 +0800

    Random products for index page

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,23 @@ const fireDb = require('../models/firebaseAdmin');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
-  res.render('index');
+  
+  fireDb.ref('products').once('value').then((snap) => {
+    let data = [];
+    snap.forEach((item) => {
+      data.push(item.val());
+    });
+    let i, j, temp;
+    for (i = data.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      temp = data[i];
+      data[i] = data[j];
+      data[j] = temp;
+    }
+    res.render('index', {
+      indexItem: data.slice(0, 3)
+    });
+  });  
 });
 
 router.get('/register', function (req, res) {
