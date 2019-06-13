93febe31643d38d2a6aa19dc1e0d94a3ac0eac08
a7711555 <toby19940927@gmail.com>
2019-06-13 16:58:45 +0800

    add csurf package

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const flash = require('connect-flash');
+const csrf = require('csurf');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -19,7 +22,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(flash());
+app.use(csrf());
 
+// routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productRouter);
@@ -37,7 +48,9 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    msg: err
+  });
 });
 
 module.exports = app;
